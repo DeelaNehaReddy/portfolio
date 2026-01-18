bc7123911e8a6c27ff6a990cc39558e5d5ffcac0
DeelaNehaReddy <deelanehareddythummuru@gmail.com>
2026-01-18 09:25:28 +0530

    Cleaned folder structure and updated project files

---
+++ src/portfolio.js
@@ -0,0 +1,393 @@
+const settings = {
+  isSplash: false,
+useCustomCursor: false,
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hey there.",
+  title2: "Deela Neha Reddy",
+  logo_name: "DeelaNehaReddyT",
+  nickname: "neha",
+  full_name: "Deela Neha Reddy",
+  subTitle:
+    "Aspiring Full-Stack Developer. Turning ideas into code. Growing every day.",
+  resumeLink:
+    "https://drive.google.com/file/d/1WUkWpvujmmF-AUo_K64SnaveDl8nE5sY/view?usp=sharing",
+  mail: "mailto:deelanehareddythummuru@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/DeelaNehaReddy",
+  linkedin: "https://www.linkedin.com/in/deelanehareddy-thummuru/",
+  gmail: "deelanehareddythummuru@gmail.com",
+  leetcode: "https://leetcode.com/u/DeelaNehaReddyThummuru/",
+  geeksforgeeks: "https://www.geeksforgeeks.org/profile/deelanehared58xx",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Skills",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive and accessible web interfaces using modern HTML, CSS, and JavaScript",
+        "⚡ Developing full-stack web applications using React, Node.js, and Express.js",
+        "⚡Strong foundation in Data Structures and Algorithms with consistent problem-solving practice",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+{
+  skillName: "Express.js",
+  fontAwesomeClassname: "simple-icons:express",
+  style: { color: "#FFFFFF" },
+},
+  
+      {
+  skillName: "Node.js",
+  fontAwesomeClassname: "simple-icons:nodedotjs",
+  style: {
+    color: "#339933",
+  },
+},
+{
+  skillName: "Java",
+  fontAwesomeClassname: "devicon:java",
+  style: {
+    color: "#007396",
+  },
+},
+            {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },   
+
+        {
+  skillName: "MySQL",
+  fontAwesomeClassname: "simple-icons:mysql",
+  style: { color: "#4479A1" },
+},
+
+
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+{
+  skillName: "GitHub",
+  fontAwesomeClassname: "simple-icons:github",
+  style: { color: "#E6E6E6" },
+},
+{
+  skillName: "VS Code",
+  fontAwesomeClassname: "simple-icons:visualstudiocode",
+  style: {
+    color: "#007ACC",
+  },
+}
+
+      ],
+    },
+    
+  ],
+};
+const competitiveSites = {
+  competitiveSites: [
+    {
+      siteName: "LeetCode",
+      iconifyClassname: "simple-icons:leetcode",
+      style: {
+        color: "#F79F1B",
+      },
+      profileLink: "https://leetcode.com/u/DeelaNehaReddyThummuru/",
+    },
+    {
+      siteName: "HackerRank",
+      iconifyClassname: "simple-icons:hackerrank",
+      style: {
+        color: "#2EC866",
+      },
+      profileLink: "https://www.hackerrank.com/profile/23pa1a12l7",
+    },
+    {
+      siteName: "Geeks For Geeks",
+      iconifyClassname: "simple-icons:geeksforgeeks",
+      style: {
+        color: "green",
+      },
+      profileLink: "https://www.geeksforgeeks.org/profile/deelanehared58xx",
+    },
+  ],
+};
+const degrees = {
+  degrees: [
+    {
+      title: "Vishnu Institute of Technology, Bhimavaram",
+      subtitle: "B.Tech in Information Technology",
+      logo_path: "vishnu1.png",
+      alt_name: "DAL",
+      duration: "2023 - 2027",
+      descriptions: [
+        "⚡ Strong foundation in OOP, DBMS, CN, and Software Engineering.",
+        "⚡ Improved problem-solving skills through projects and practical work.",
+        "⚡Passionate about learning new technologies and building real-world solutions.",
+      ],
+
+    },
+    {
+      title: "SR Junior College, Vijayawada",
+      subtitle: "Intermediate (MPC)",
+      logo_path: "sr1.jfif",
+      alt_name: "SR",
+      duration: "2021 - 2023",
+      descriptions: [
+        "⚡ Secured College First position with an overall score of 98%.",
+        "⚡  Built strong analytical and logical thinking skills through mathematics-focused learning.",
+        "⚡ Built interest in technology and programming, which motivated me to pursue engineering",
+      ],
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+     { 
+      title: "Career Development 2x: Communication and Teamwork",
+      subtitle: "edX",
+      logo_path: "edx.png",
+      certificate_link: "https://courses.edx.org/certificates/22a3d7294f0a47408e09ab51aedddc29",
+      alt_name: "edX",
+      color_code: "#ffc475",
+    },
+     { 
+      title: "CSX0001: Computer Science 101",
+      subtitle: "StanfordOnline",
+      logo_path: "big_stanford-online.png",
+      certificate_link: "https://courses.edx.org/certificates/33b03b485f37437e8e27b38b70801685",
+      alt_name: "StanfordOnline",
+      color_code: "#ffc475",
+    },
+    {
+      title: "SIH Internal Hackathon 2024",
+      subtitle: "SIH",
+      logo_path: "sihlogo.avif",
+      certificate_link:
+        "https://drive.google.com/file/d/1Px-WuRSognfKfiJqDr_eyf3DzD957cGw/view?usp=sharing",
+      alt_name: "SIH",
+      color_code: "#ffc475",
+    },
+    {
+      title: "MongoDB Developer's Toolkit",
+      subtitle: "GeeksforGeeks",
+      logo_path: "GG_Logo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1yp_vn8kJ4BstD-X1EDe_-uT7YbYPMCo4/view?usp=sharing",
+      alt_name: "GFG",
+      color_code: "#ffc475",
+    },
+    { 
+      title: "Python for Data Science",
+      subtitle: "NPTEL",
+      logo_path: "nptellogo.webp",
+      certificate_link: "https://drive.google.com/file/d/1eZD9nZQWY7eAmTN2LoemhwV1XiYkiWry/view?usp=sharing",
+      alt_name: "NPTEL",
+      color_code: "#ffc475",
+    },
+    { 
+      title: "Cyber Security and Privacy",
+      subtitle: "NPTEL",
+      logo_path: "nptellogo.webp",
+      certificate_link: "https://drive.google.com/file/d/1h6rhI3s21Xd3KJVTTQoc-xGx3CrCTKhT/view?usp=sharing",
+      alt_name: "NPTEL",
+      color_code: "#ffc475",
+    },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Current Job and Internship",
+  //subtitle: "Current Job and Internship",
+  description:
+    "I have collaborated with teams and individuals as a full-stack web developer, specializing in building scalable applications using React, Node.js, and MongoDB. My expertise includes creating dynamic, user-friendly interfaces, optimizing backend systems, and ensuring smooth integration between frontend and backend components.",
+  header_image_path: "exper.svg",
+  sections: [
+    {
+      title: "Current Job",
+      experiences: [
+        {
+          title: "Bachleor of Technology in Information Technology Student",
+          company: "Vishnu Institute of Technology",
+          company_url: "https://vishnu.edu.in/",
+          logo_path: "vishnu1.png",
+          duration: "2023 - Present",
+          location: "Bhimavaram, Andhra Pradesh",
+          description:
+            "Currently pursuing a Bachelor of Technology in Information Technology at Vishnu Institute of Technology. Passionate about full-stack web development, focusing on building scalable applications and innovative solutions while exploring advancements in modern web technologies.",
+          color: "#000000",
+        },
+      ],
+    },
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Full-Stack Developer Intern",
+          company: "SURE ProEd",
+          company_url: "https://www.suretrustforruralyouth.com/",
+          logo_path: "suretrust.png",
+          duration: "October 2025 - Present",
+          location: "Work From Home",
+          description:
+            "Full-Stack Web Developer Intern at SureProEd, contributing to the development of scalable web applications with React, Node.js, focusing on performance, usability, and clean code.",
+          color: "#ee3c26",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:"My projects showcase a blend of innovation and technical expertise by combining modern full-stack web development with strong problem-solving skills. I specialize in building scalable applications using React for intuitive user interfaces and Node.js for secure, efficient backends. With a focus on clean architecture and user-centric design, my work highlights both technical excellence and real-world applicability.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "neha.png",
+    description:
+      "I’d love to hear from you! Whether you have questions, collaboration ideas, or just want to connect, feel free to reach out. Let’s create something amazing together! Drop me a message below, and I’ll get back to you as soon as possible.",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      name: "Skill Rise",
+      url: "https://sih-nine-theta.vercel.app/",
+      description:
+        "SkillRise connects India’s rural talent with real job opportunities – empowering skilled workers to showcase, grow, and shine.",
+      languages: [
+         {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+       {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Node.js",
+          iconifyClass: "devicon-nodejs",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+      ],
+    },
+     {
+      name: "Sports Scheduler",
+      url: "https://sports-gameplan.netlify.app/",
+      description:
+        "GamePlan – Smarter Sports Scheduling, Made Simple Organize matches, manage teams, and track schedules — all in one place.",
+      languages: [
+        {
+          name:"HTML5",
+          iconifyClass: "devicon-html5",
+        },
+        {
+          name:"CSS3",
+          iconifyClass: "devicon-css3",
+        },
+       {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Node.js",
+          iconifyClass: "devicon-nodejs",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "logos-mongodb",
+        },
+      ],
+    },
+    {
+      name: "Micro Hustle",
+      url: "https://github.com/DeelaNehaReddy/MicroHustle",
+      description: "A discovery platform to browse, explore, and evaluate micro hustle opportunities",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Node.js",
+          iconifyClass: "devicon-nodejs",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "logos-mongodb",
+        },
+      ],
+    },
+    
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  competitiveSites,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+  
+};
